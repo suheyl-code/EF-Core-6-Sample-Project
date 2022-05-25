d635d27d755e034a6667e5dedf3c5e5ce46fbacc
Suheyl Esadli <79663047+suheyl-code@users.noreply.github.com>
2022-05-25 05:22:35 +0300

    enhanced

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -7,13 +7,42 @@ using (PubContext context = new PubContext())
 {
     context.Database.EnsureCreated();
 }
-GetAuthors();
-AddIAuthor();
-GetAuthors();
+
+SequenceOfSteps();
+
+/// Program Start point method
+void SequenceOfSteps()
+{
+    //GetAuthors();
+    //AddIAuthor();
+    //GetAuthors();
+    //AddAuthorWithBooks();
+    GetAuthorWithBooks();
+}
+
+void AddAuthorWithBooks()
+{
+    var author = new Author { FirstName = "Julie", LastName = "Larmen" };
+    author.Books.Add(new Book { AuthorId = 2, Title = "Entity Framework 6 Basics", PublishDate = new DateTime(2019, 1, 12) });
+    author.Books.Add(new Book { AuthorId = 2, Title = "Entity Framework 6 Advanced", PublishDate = new DateTime(2021, 3, 20) });
+
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+
+void GetAuthorWithBooks()
+{
+    using var context = new PubContext();
+    var books = context.Books.ToList();
+    var author = context.Authors.ToList();
+    books.ForEach(x => Console.WriteLine($"{x.Author.FirstName} {x.Author.LastName}: {x.Title} --- {x.PublishDate}"));
+}
 
 void AddIAuthor()
 {
-    var author = new Author() { FirstName = "Mehmeh", LastName = "Babaoğlu" };
+    var author = new Author() { FirstName = "Mehmet", LastName = "Babaoğlu" };
     using var context = new PubContext();
     context.Authors.Add(author);
     context.SaveChanges();
